707fce7bc141d7f252e9acee6624657cfc850807
Guilherme Luan <guilhermeluan@pm.me>
2024-10-30 14:22:46 -0300

    feat: create project

---
+++ src/main/java/dev/guilhermeluan/SistemaListaTarefaApplication.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaListaTarefaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaListaTarefaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/guilhermeluan/SistemaListaTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaListaTarefaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
