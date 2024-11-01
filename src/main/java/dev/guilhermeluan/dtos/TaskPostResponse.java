674ca197a58e9c7047f56a515cf64ba4094a4210
Guilherme Luan <guilhermeluan@pm.me>
2024-11-01 18:49:56 -0300

    fix: dateLimite now returns 'yyyy-MM-dd' pattern

--- src/main/java/dev/guilhermeluan/dtos/TaskPostResponse.java
+++ src/main/java/dev/guilhermeluan/dtos/TaskPostResponse.java
@@ -1,11 +1,14 @@
 package dev.guilhermeluan.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public record TaskPostResponse(
         Long id,
         String taskName,
         Double cost,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         Date dataLimit,
         Integer presentationOrder
 ) {
