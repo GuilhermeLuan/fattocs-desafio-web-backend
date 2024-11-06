5bfb714268cced5076cc2f338985c06b9546e7f0
Guilherme Luan <guilhermeluan@pm.me>
2024-11-06 12:10:50 -0300

    feat: and envTemplate

--- src/main/java/dev/guilhermeluan/service/TaskService.java
+++ src/main/java/dev/guilhermeluan/service/TaskService.java
@@ -7,8 +7,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
