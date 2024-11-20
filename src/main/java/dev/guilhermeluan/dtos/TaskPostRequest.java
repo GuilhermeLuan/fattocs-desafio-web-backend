1a04b15f0d6283f6f45ac6f519ada54fac199a96
Guilherme Luan <guilhermeluan@pm.me>
2024-11-20 11:49:22 -0300

    fix: wrong msg for cost validation

--- src/main/java/dev/guilhermeluan/dtos/TaskPostRequest.java
+++ src/main/java/dev/guilhermeluan/dtos/TaskPostRequest.java
@@ -10,7 +10,7 @@ public record TaskPostRequest(
         @NotBlank(message = "The field 'taskName' is required")
         String taskName,
         @NotNull(message = "The field 'cost' is required")
-        @DecimalMax(value = "999999999999999.00", message = "O custo não pode ser maior que 999999999999999.")
+        @DecimalMax(value = "999999999999999.00", message = "The cost cannot be greater than 999999999999999.")
         Double cost,
         @NotNull(message = "The field 'dataLimit' is required")
         Date dataLimit
