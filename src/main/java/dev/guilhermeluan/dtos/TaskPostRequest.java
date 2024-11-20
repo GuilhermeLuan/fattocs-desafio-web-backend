e5afcb949797d49b767ed77de027f8a870f7b6d8
Guilherme Luan <guilhermeluan@pm.me>
2024-11-20 11:44:19 -0300

    feat: cost validation

--- src/main/java/dev/guilhermeluan/dtos/TaskPostRequest.java
+++ src/main/java/dev/guilhermeluan/dtos/TaskPostRequest.java
@@ -1,5 +1,6 @@
 package dev.guilhermeluan.dtos;
 
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,6 +10,7 @@ public record TaskPostRequest(
         @NotBlank(message = "The field 'taskName' is required")
         String taskName,
         @NotNull(message = "The field 'cost' is required")
+        @DecimalMax(value = "999999999999999.00", message = "O custo não pode ser maior que 999999999999999.")
         Double cost,
         @NotNull(message = "The field 'dataLimit' is required")
         Date dataLimit
